e279b4499ecaac94446c6e5dc10152cfa2556569
PremRawat <premrawat9873@gmail.com>
2025-08-17 00:47:32 +0530

    second commit

--- Frontend/src/components/bottom.jsx
+++ Frontend/src/components/bottom.jsx
@@ -4,5 +4,6 @@ export default function Bottom({text,link,url}){
         <h1 className="text-white text-xl mb-4 text-center pr-1">{text}</h1>
         <a href={url} className="text-blue-500 text-xl mb-4 text-center underline decoration-blue-500  underline-offset-2">{link}</a>
         </>
+        //umm
     )
 }
