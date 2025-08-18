685573d7a191e2d0f157e061fcc40b39c42fc62d
PremRawat <premrawat9873@gmail.com>
2025-08-19 00:34:24 +0530

    commitingg

--- Frontend/src/components/Web Component/signup.jsx
+++ Frontend/src/components/Web Component/signup.jsx
@@ -1,17 +1,33 @@
 import InputField from "../input"
-import { useState } from "react"
+import {useRecoilState,useRecoilValue} from "recoil";
 import Button from "../button";
 import Heading from "../heading";
 import Subheading from "../subheading";
 import Bottom from "../bottom";
+import { emailState,passwordState,firstNameState,lastNameState } from "../atoms/signupAtoms";
+import axios from "axios";
+
 
 
 
 export default function Signup({Url}){
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");   
+  const email = useRecoilValue(emailState);
+  const password = useRecoilValue(passwordState);
+  const firstName = useRecoilValue(firstNameState);
+  const lastName = useRecoilValue(lastNameState);
+  const handleSignup = async () => {
+  try {
+    const res = await axios.post("http://localhost:3000/api/v1/users/signup",{
+      username: email,
+      password,
+      firstName,
+      lastName
+    });
+
+  } catch (err) {
+    console.error("Signup failed:", err);
+  }
+};
     return(
         <>
 
@@ -29,22 +45,11 @@ export default function Signup({Url}){
                     </div>
 
                     <div className="pl-4 pr-4 space-y-3">
-                    <InputField heading="First Name" placeholder={"First Name"} type={"text"} onChange={e=>{
-                        setFirstName(e.target.value);
-                    }}/>
-                    <InputField heading="Last Name" placeholder={"Last Name"} type={"text"} onChange={e=>{
-                        setLastName(e.target.value);
-                    }}/>
-                    <InputField heading="Email" placeholder={"Email"} type={"email"} onChange={e=>{
-                        setEmail(e.target.value);
-                    }}/>
-                    <InputField heading="Password" placeholder={"Password"} type={"password"} onChange={e=>{
-                        setPassword(e.target.value);
-                    }}/>
+                        <Inputs/>
                     </div>
 
                     <div className="flex justify-center items-center pt-4">
-                        <Button text={"Sign Up"}/>
+                        <Button text={"Sign Up"} onClick={handleSignup}/>
                     </div>
                     <div className="flex justify-center items-center ">
                         <Bottom text={"Already have an account?"} link={"Sign In"} url={"/signin"}/>
@@ -52,6 +57,45 @@ export default function Signup({Url}){
  
             </div>
         </div>
+        </>
+    )
+}
+
+function Inputs(){
+    const [firstName, setFirstName] = useRecoilState(firstNameState);
+    const [lastName, setLastName] = useRecoilState(lastNameState);
+    const [email, setEmail] = useRecoilState(emailState);
+    const [password, setPassword] = useRecoilState(passwordState);
+    return (
+        <>
+        <InputField
+        heading="First Name"
+        placeholder="First Name"
+        type="text"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <InputField
+        heading="Last Name"
+        placeholder="Last Name"
+        type="text"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <InputField
+        heading="Email"
+        placeholder="Email"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <InputField
+        heading="Password"
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
         </>
     )
 }
